b914c32edc179d7308fdb6f73fd5931b77c2a4ab
diegopablomansilla <diegopablomansilla@gmail.com>
2016-09-21 12:21:23 -0300

    Armando el workspace eclipse y proyecto java

---
+++ ws_cendrabpm_api/cendrabpmapi/src/main/java/org/cendra/bpm/CendraBpmApiApplication.java
@@ -0,0 +1,12 @@
+package org.cendra.bpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CendraBpmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CendraBpmApiApplication.class, args);
+	}
+}
---
+++ ws_cendrabpm_api/cendrabpmapi/src/test/java/org/cendra/bpm/CendraBpmApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cendra.bpm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CendraBpmApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
